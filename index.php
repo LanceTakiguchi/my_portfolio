7949c7e002a35db49dd8516c3e9fe87526bf3531
ltakiguc <ltakiguc@uci.edu>
2016-11-02 17:56:17 -0700

    Barebones html created

---
+++ index.php
@@ -0,0 +1,36 @@
+<!-- *********** 
+Project Name: My Portfolio
+File Name: index.php
+Author: Lance Takiguchi
+Created: 11/02/2016 Time: 16:23 
+Objective: My online portfolio. Displays my web developement skils through projects I have completed
+Version: 0.1
+Version Objective: Create a basic index page for the website
+*********** -->
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="utf-8"> 
+	<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+	<meta name="keywords" content="PHP, CSS, HTML, JS, Javascript, firebase, bootstrap, angular.js, home, Lance Takiguchi, portfolio, resume, projects, index, website, homepage, landing page, web development, learning fuze, application, apps, website, webpage">
+	<meta name="author" content="Lance Takiguchi">
+	<meta name="description" content="Lance Takiguchi's Portfolio Website">
+	<meta name="date" content="11/02/16, November 02, 2016">
+	<link rel="icon" href="assets/images/cirlce_check.png">
+	<link rel="stylesheet" type="text/css" href="assets/styles/style.css">
+	<title> Lance Takiguchi | Portfolio</title>
+</head>
+<body>
+	<h1> Lance Takiguchi </h1>
+	<nav class="navbar">
+		<ul>
+			<li> Home </li>
+			<li> About </li>
+			<li> Projects </li>
+			<li> Contact </li>
+		</ul>
+	</nav>
+	<p> Welcome </p>
+	<img src="assets/images/awesome.jpg">
+</body>	
+</html>
