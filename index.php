e9e74bcca5dc5060ba2bd0428e57a2714d62f689
ltakiguc <ltakiguc@uci.edu>
2016-11-16 19:43:33 -0800

    About, Skills, contact sections filled

--- index.php
+++ index.php
@@ -33,24 +33,49 @@ Version Date: 11/09/2016 Time: 14:17
 			<li> <a href="#applications"> Applications </a> </li>
 			<li> <a href="#skills"> Skills </a> </li>
 			<li> <a href="#contact"> Contact </a> </li>
-			<li> </li>
 		</ul>
 	</nav>
 	
-	<div id="about" class="set_height">
-		<h2 class="title"> Welcome </h2>
+	<div id="about" class="set_height margin_center navbar_avoid_padding">
+		<div id="quote">
+			<p>Astronomy compels the soul to look upward, and leads us from this world to another.</p>
+			<p> - <span class="italic">Plato</span>, The Republic, <span class="italic"> 343 BCE.</span></p>
+		</div>
 	</div>
-	<div id="applications" class="set_height">
+	<div id="applications" class="set_height margin_center navbar_avoid_padding">
 		<h2 class="title"> Applications </h2>
 		<p> Nullam semper laoreet viverra. Morbi sed cursus libero. Sed eu facilisis arcu. Nullam euismod luctus orci nec ullamcorper. Fusce et nunc laoreet, elementum justo id, feugiat ligula. Donec porttitor diam vel eros consectetur, sed lacinia lorem euismod. Integer sit amet pellentesque nibh. Mauris lacinia, tortor ut malesuada faucibus, arcu libero sagittis nunc, ut suscipit urna augue sit amet odio. Curabitur rutrum quam a condimentum lacinia. Curabitur dignissim felis a ullamcorper dictum. Nulla non placerat quam. Quisque mauris mauris, malesuada rhoncus lorem nec, mattis auctor tellus. Sed vel purus odio. </p>
 	</div>
-	<div id="skills" class="set_height">
+	<div id="skills" class="set_height margin_center navbar_avoid_padding">
 		<h2 class="title"> Skills </h2>
-		<p> Sed interdum iaculis metus, vel convallis felis accumsan sed. Nulla a egestas erat. Proin faucibus, nunc et malesuada facilisis, erat mi fermentum nisl, ac dapibus leo justo eget erat. Fusce laoreet eros nisi, id maximus quam efficitur ac. Suspendisse suscipit nibh blandit ante tempor dictum. Aliquam at pharetra magna. Aenean justo erat, tempus vitae semper eu, pharetra eu dui. Mauris quis aliquam lacus. </p>
+		<div class="code">
+			<p> var <span class="code_name">education</span> = { </p><br>
+			<p class="indent_1"> <span class="code_name">learning_fuze: "<span class="code_value">Web Development</span>",</span></p><br>
+			<p class="indent_1"> <span class="code_name">uc_irvine: "<span class="code_value">Informatics</span>"</span></p><br>
+			<p> }; </p> <br>
+			<p> var <span class="code_name">work_experince</span> = { </p><br>
+			<p class="indent_1"> <span class="code_name">learning_fuze: "<span class="code_value">Code Review</span>"</span>,</p><br>
+			<p> }; </p> <br>
+			<p> var <span class="code_name">skills</span> = [ </p><br>
+			<p class="indent_1"> <span class="code_name">"<span class="code_value">HTML</span>",
+				"<span class="code_value">CSS</span>",
+				"<span class="code_value">Javascript</span>", 
+				"<span class="code_value">jQuery</span>", 
+				"<span class="code_value">git</span>"
+				"<span class="code_value">AngularJS</span>", 
+				"<span class="code_value">Firebase</span>"
+				"<span class="code_value">Bootstrap</span>"
+				"<span class="code_value">PHP</span>"
+			</span> </p><br>
+			<p> ]; </p> 
+		</div>	
 	</div>
-	<div id="contact" class="set_height">
+	<div id="contact" class="set_height margin_center navbar_avoid_padding">
 		<h2 class="title"> Contacts </h2>
-		<p> In nec faucibus augue, vitae auctor massa. Cras eget eros vestibulum, sodales risus eu, feugiat felis. Duis elementum eros consectetur ultrices euismod. Pellentesque mollis, massa ac lobortis imperdiet, nisl metus commodo eros, ac porta quam libero quis ligula. Suspendisse iaculis purus eu aliquam porttitor. Praesent vel risus imperdiet, accumsan urna at, convallis risus. Vestibulum ornare mollis sapien, id faucibus ex dignissim et. Integer non diam risus. Duis ornare purus non ante fringilla gravida. Nulla lorem risus, bibendum vel elementum in, tincidunt vitae sem. Vestibulum hendrerit metus quis lorem placerat, eget ornare dolor feugiat. Nullam quis mi aliquet, vulputate massa sed, laoreet ante. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vivamus euismod arcu diam. </p>
+		<p> Email: LanceTakiguchi@gmail.com </p>
+		<p> Phone: (909) 815-1863 </p>
+		<p> LinkedIn:  <a>linkedin.com/in/lancetakiguchi</a></p>
+		<p> Github:  <a>github.com/LanceTakiguchi</a></p>
 	</div>
 </body>	
 </html>
---
+++ js/test.js
@@ -0,0 +1,8 @@
+var education = {
+	learningfuze: "Web Developement",
+	UC Irvine: "Informatics"
+}
+
+var skills = [
+	"Javascript", "cookies"
+]
