d4f082000f3b055d441b7c360acd59addd7fd184
ltakiguc <ltakiguc@uci.edu>
2017-01-10 14:05:05 -0800

    Solarized color scheme: Orange, white, grey. Fonts: Crimson Text with Poiret One

--- index.php
+++ index.php
@@ -20,7 +20,7 @@ Version Date: 01/04/2017 Time: 16:49
 	<meta name="date" content="11/02/16, November 02, 2016">
 	<link rel="icon" href="assets/images/sun_icon.png">
 	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-	<!-- <link href="https://fonts.googleapis.com/css?family=Poiret+One|Raleway" rel="stylesheet"> -->
+	<link href="https://fonts.googleapis.com/css?family=Poiret+One" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Crimson+Text|Lora|Raleway" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="css/style.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
