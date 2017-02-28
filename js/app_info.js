033f23f6ac8c7b117c62ffe8d528b9755735d7cb
ltakiguc <ltakiguc@uci.edu>
2017-02-28 11:11:01 -0800

    Improved app hover message reset

--- js/app_info.js
+++ js/app_info.js
@@ -1,18 +1,28 @@
 $(document).ready(function() {
-	$("#app_sgt").hover(function(event) {
+	$("#app_sgt").mouseover(function(event) {
 		description("A grading application for teachers to input student grades");
 	});
-	$("#app_swrf").hover(function(event) {
-		description("A javascript game with Google Login and ecommerce API");
+	$("#app_sgt").mouseout(function(){
+		
+		description("Hover over an app to see it's description"); // Keep it the same as the hardcoded HTML")
+});
+	$("#app_swrf").mouseover(function(event) {
+		description("A team project javascript game with Google Login and ecommerce API");
 	});
-	$("#app_calc").hover(function(event) {
+	$("#app_swrf").mouseout(function(){
+		description("Hover over an app to see it's description"); // Keep it the same as the hardcoded HTML")
+});
+	$("#app_calc").mouseover(function(event) {
 		description("A javascript calculator with a complete history");
 	});
-	$("#applications").hover(function(event) {
-		description("Hover over an app to see it's description"); // Keep it the same as the hardcoded HTML
-	});
-	function description (message){
-		var info = $("#app_info p");
-		info.text(message)
-	}
+	$("#app_calc").mouseout(function(){
+		description("Hover over an app to see it's description"); // Keep it the same as the hardcoded HTML")
+});
+	// $("#applications").hover(function(event) {
+	// 	description("Hover over an app to see it's description"); // Keep it the same as the hardcoded HTML
+	// });
+function description (message){
+	var info = $("#app_info p");
+	info.text(message)
+}
 });
