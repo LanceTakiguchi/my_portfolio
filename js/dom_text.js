e25daeb7be58aec2c14136fa884a4c9c278ade82
Lance Takiguchi <lancetakiguchi@gmail.com>
2018-06-15 17:05:48 -0700

    navBar text changed to load via handleBars

---
+++ js/dom_text.js
@@ -0,0 +1,10 @@
+var domText = {
+    testing: "Does this work?",
+    navBar: {
+        home: "Home",
+        about: "About",
+        applications: "Applications",
+        skills: "Skills",
+        contact: "Contact"
+    }
+};
