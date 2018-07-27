8f10215c673df7d397130f5736441ade40810275
Lance Takiguchi <lancetakiguchi@gmail.com>
2018-07-27 07:44:37 -0700

    Dom texts now loaded via handlebars

--- js/dom_text.js
+++ js/dom_text.js
@@ -1,4 +1,12 @@
 var domText = {
+    pictures: {
+        externalProfile: "https://media.licdn.com/dms/image/C5603AQGQ2whLWp1i4Q/profile-displayphoto-shrink_200_200/0?e=1538006400&v=beta&t=gcsEDjsO6b8NeT_ho31DG7fwwfVQ5GxZlC0EALlnED4",
+        profile: "assets/images/profile_pic.jpg",
+        icon: "assets/images/sun_icon.png"
+    },
+    planets: {
+        orbit: "Orbit"
+    },
     navBar: {
         home: "home",
         about: "about",
@@ -6,52 +14,58 @@ var domText = {
         skills: "skills",
         contact: "contact"
     },
+    landing: {
+        quote: "Who made the Bear and Orion, the Pleiades and the chambers of the south",
+        credit: "Job 9:9"
+    },
     me: {
         name: "Lance Takiguchi",
         about: {
             p1: "Hello! The name is Lance, and welcome to my portfolio. I am a web developer in Irvine, California who loves tackling the puzzles hidden in code.",
             p2: "I am always looking for jobs to feed my restless thoughts. I enjoy implementing designs that capture a user's attention, the behind the scenes action is where I like to fascinate the star-gazer.",
             p3: "I am currently avaliable for web development work around Orange County area, as well as remote or freelance work."
-        }
+        },
+        profileAlt: "Just imagine a really handsome web developer"
     },
     apps: {
         sgt: {
             name: "Student Grade Table",
             ref: "sgt",
-            githubLink: "https://github.com/LanceTakiguchi/portfolio_SGT",
-            liveLink: ""
+            githubLink: "https://github.com/LanceTakiguchi/sgt",
+            liveLink: "https://lancetakiguchi.github.io/sgt/"
         },
         swrf: {
             name: "Rogue Fighter",
             ref: "swrf",
-            githubLink: "https://github.com/LanceTakiguchi/portfolio_Rogue_Fighter",
-            liveLink: ""
+            githubLink: "https://github.com/LanceTakiguchi/roguefighter",
+            liveLink: "https://lancetakiguchi.github.io/roguefighter/"
         },
         calc: {
             name: "Calculator",
             ref: "calc",
-            githubLink: "https://github.com/LanceTakiguchi/portfolio_SGT",
-            liveLink: ""
+            githubLink: "https://github.com/LanceTakiguchi/calc",
+            liveLink: "https://lancetakiguchi.github.io/calc/"
         },
         ttwars: {
             name: "Tic Tac Wars",
             ref: "ttwars",
-            githubLink: "https://github.com/LanceTakiguchi/portfolio_SGT",
+            githubLink: "https://github.com/LanceTakiguchi/https://github.com/LanceTakiguchi/portfolio_tictacwars",
             liveLink: ""
         },
         nfl: {
             name: "NFL Now",
             ref: "nfl",
-            githubLink: "https://github.com/LanceTakiguchi/portfolio_SGT",
+            githubLink: "https://github.com/LanceTakiguchi/NFLNow",
             liveLink: ""
         },
         ship: {
             name: "Shipping Calculator",
             ref: "ship",
-            githubLink: "https://github.com/LanceTakiguchi/portfolio_SGT",
+            githubLink: "https://github.com/LanceTakiguchi/portfolio_shipping_calculator",
             liveLink: ""
         }
     },
+    appsInfo: "Hover over an app to see it's description",
     skills: {
         experince: {
             name: "Experience",
@@ -88,10 +102,10 @@ var domText = {
                 "Git",
                 "Github",
                 "Chrome DevTools",
-                "MAMP",
                 "Jira",
                 "MeisterTask",
                 "Slack",
+                "MAMP",
                 "Postman"                
             ]
         }
@@ -102,7 +116,12 @@ var domText = {
             email: "Email",
             message: "Message"
         },
-        submit: "Submit"
+        submit: "Submit",
+        links: {
+            linkedin: "https://www.linkedin.com/in/lancetakiguchi",
+            github: "https://github.com/LanceTakiguchi",
+            resume: "https://drive.google.com/open?id=1xNkA7XShgGez3t4Itl0NQk9VMNhnVuAf"
+        }
     }
 };
 $(document).ready(function () {
