9af64dd6177794be312d7c121de518e51e8e6084
Lance Takiguchi <lancetakiguchi@gmail.com>
2018-10-13 19:09:54 -0700

    planets corrected orbits periods

--- js/orbit_start.js
+++ js/orbit_start.js
@@ -38,12 +38,12 @@
  * - 87.969 days * 60 seconds / 365.2422 days = 14.45106835957072 seconds for 1 mercury orbit
  * - 14.45106835957072 seconds * 161.8 degrees / 360 degrees = 6.494952390495951 seconds
  */
-var mercuryOP = 87.969;  // Number of Earth days to orbit sun
-var mercuryAngle = Math.atan(0.1982899/0.2503749) * (180 / Math.PI) + 180;
-var mercuryOPAnimation = 14.45106835957072 // # of seconds for mercury's animation to orbit
-var mercuryAnimationStart = mercuryAngle / 360 * mercuryOPAnimation //# of seconds into mercury's animation orbit on 1/1/1970; the starting position for mercury
-var mercuryDay = setDate % mercuryOP; // # of earth days mercury is into its 87.969 earth days to orbit sun - CHECK against actual day into orbit
-var mercuryAnimationDelay = (mercuryDay * mercuryOPAnimation / mercuryOP + mercuryAnimationStart) % mercuryOPAnimation; // # of seconds into mercury's orbit animation to be in today's actual mercury position + 3.2 seconds of position compensation
+// var mercuryOP = 87.969;  // Number of Earth days to orbit sun
+// var mercuryAngle = Math.atan(0.1982899/0.2503749) * (180 / Math.PI) + 180;
+// var mercuryOPAnimation = 14.45106835957072 // # of seconds for mercury's animation to orbit
+// var mercuryAnimationStart = mercuryAngle / 360 * mercuryOPAnimation //# of seconds into mercury's animation orbit on 1/1/1970; the starting position for mercury
+// var mercuryDay = setDate % mercuryOP; // # of earth days mercury is into its 87.969 earth days to orbit sun - CHECK against actual day into orbit
+// var mercuryAnimationDelay = (mercuryDay * mercuryOPAnimation / mercuryOP + mercuryAnimationStart) % mercuryOPAnimation; // # of seconds into mercury's orbit animation to be in today's actual mercury position + 3.2 seconds of position compensation
 
 /**
  *  *** Venus ***
@@ -55,12 +55,12 @@ var mercuryAnimationDelay = (mercuryDay * mercuryOPAnimation / mercuryOP + mercu
  * - 224.701 days * 60 seconds / 365.2422 days = 36.91265686166604 seconds for 1 venus orbit
  * - 36.91265686166604 seconds * 161.8 degrees / 360 degrees = 6.494952390495951 seconds
  */
-var venusOP = 224.701;  // Number of Earth days to orbit sun
-var venusAngle = -Math.atan(-0.0345935/-0.7259848) * (180 / Math.PI) + 90;
-var venusOPAnimation = 36.91265686166604 // # of seconds for venus's animation to orbit
-var venusAnimationStart = venusAngle / 360 * venusOPAnimation //# of seconds into venus's animation orbit on 1/1/1970; the starting position for mercury
-var venusDay = setDate % venusOP; // # of earth days venus is into its 224.701 earth days to orbit sun - CHECK against actual day into orbit
-var venusAnimationDelay = (venusDay * venusOPAnimation / venusOP + venusAnimationStart) % venusOPAnimation; // # of seconds into mercury's orbit animation to be in today's actual mercury position + 3.2 seconds of position compensation
+// var venusOP = 224.701;  // Number of Earth days to orbit sun
+// var venusAngle = -Math.atan(-0.0345935/-0.7259848) * (180 / Math.PI) + 90;
+// var venusOPAnimation = 36.91265686166604 // # of seconds for venus's animation to orbit
+// var venusAnimationStart = venusAngle / 360 * venusOPAnimation //# of seconds into venus's animation orbit on 1/1/1970; the starting position for mercury
+// var venusDay = setDate % venusOP; // # of earth days venus is into its 224.701 earth days to orbit sun - CHECK against actual day into orbit
+// var venusAnimationDelay = (venusDay * venusOPAnimation / venusOP + venusAnimationStart) % venusOPAnimation; // # of seconds into mercury's orbit animation to be in today's actual mercury position + 3.2 seconds of position compensation
 
 var angle = function(x, y){
     if (x > 0) {
@@ -97,7 +97,7 @@ var angle = function(x, y){
 var startPosition = function(orbital_period, x, y){
     var set_date = Date.now() / 86400000;
     var start_angle = angle(x, y);
-    var total_animation = orbital_period / 365.2422;
+    var total_animation = orbital_period * 60 / 365.2422;
     var start_animation = start_angle / 360 * total_animation;
     /**
      * planet_orbital_today: How many days into it's orbit it is relative to the starting position today
@@ -106,13 +106,13 @@ var startPosition = function(orbital_period, x, y){
     return -((planet_orbital_today * total_animation / orbital_period + start_animation) % total_animation);
 }  
 
-mercury_start = startPosition(87.969,0.2503749, 0.1982899);
+mercury_start = startPosition(87.969, 0.2503749, 0.1982899);
 venus_start = startPosition(224.701, -0.0345935, -0.7259848);
 var earth_start = startPosition(365.2422, -0.1792011, 0.9668216);
 var mars_start = startPosition(686.971, 1.3283899, 0.4913167);
 var jupiter_start = startPosition(4332.59, -5.0073049, -2.1528642);
 var saturn_start = startPosition(10759.22, 7.2883808, 5.6450379);
-var uranus_start = startPosition(30,688.5, -18.2095625, -1.9178999);
+var uranus_start = startPosition(30688.5, -18.2095625, -1.9178999);
 var neptune_start = startPosition(60182, -15.7424490, -25.8830229);
 
 $(document).ready(function () {
